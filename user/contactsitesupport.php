0b2ff3438be0c662f7b025b86cabe582313b4225
Marco <marco.cigna@gses.com.au>
2023-03-14 05:18:23 +0000

    Adding booking plugin

--- user/contactsitesupport.php
+++ user/contactsitesupport.php
@@ -41,7 +41,7 @@ if (!empty($CFG->supportpage)) {
 $PAGE->set_context(context_system::instance());
 $PAGE->set_url('/user/contactsitesupport.php');
 $PAGE->set_title(get_string('contactsitesupport', 'admin'));
-$PAGE->set_heading(get_string('contactsitesupport', 'admin'));
+// $PAGE->set_heading(get_string('contactsitesupport', 'admin'));
 $PAGE->set_pagelayout('standard');
 
 $renderer = $PAGE->get_renderer('user');
@@ -57,7 +57,10 @@ if ($form->is_cancelled()) {
     $data->notloggedinuser = (!$user);
     $message = $renderer->render_from_template('user/contact_site_support_email_body', $data);
 
-    if (!email_to_user(core_user::get_support_user(), $from, $subject, $message)) {
+    $replyto = $USER->email;
+
+    if (!email_to_user(core_user::get_support_user(), $from, $subject, $message, $messagehtml = '', $attachment = '', $attachname = '',
+    $usetrueaddress = true, $replyto)) {
         $supportemail = $CFG->supportemail;
         $form->set_data($data);
         $templatectx = [
@@ -75,6 +78,8 @@ if ($form->is_cancelled()) {
 }
 
 echo $OUTPUT->header();
+
+echo $OUTPUT->render_from_template('user/form', $obj);
 
 echo $output;
 
