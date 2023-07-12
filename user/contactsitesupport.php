cd7d07ba384d9f70c4ba588fe7061b4134c9d265
Marco <marco.cigna@gses.com.au>
2023-07-12 12:01:22 +1000

    Various core moodle custom code edits

--- user/contactsitesupport.php
+++ user/contactsitesupport.php
@@ -41,9 +41,13 @@ if (!empty($CFG->supportpage)) {
 $PAGE->set_context(context_system::instance());
 $PAGE->set_url('/user/contactsitesupport.php');
 $PAGE->set_title(get_string('contactsitesupport', 'admin'));
-$PAGE->set_heading(get_string('contactsitesupport', 'admin'));
+// $PAGE->set_heading(get_string('contactsitesupport', 'admin'));
 $PAGE->set_pagelayout('standard');
+//user must be logged in to access this page
+require_login();
 
+
+$user = isloggedin() && !isguestuser() ? $USER : null;
 $renderer = $PAGE->get_renderer('user');
 
 $form = new \core_user\form\contactsitesupport_form(null, $user);
@@ -57,8 +61,10 @@ if ($form->is_cancelled()) {
     $data->notloggedinuser = (!$user);
     $message = $renderer->render_from_template('user/contact_site_support_email_body', $data);
 
-    if (!email_to_user(core_user::get_support_user(), $from, $subject, $message)) {
-        $supportemail = $CFG->supportemail;
+    $replyto = $USER->email;
+
+    if (!email_to_user(core_user::get_support_user(), $from, $subject, $message, $messagehtml = '', $attachment = '', $attachname = '',
+    $usetrueaddress = true, $replyto)) {        $supportemail = $CFG->supportemail;
         $form->set_data($data);
         $templatectx = [
             'supportemail' => $user ? html_writer::link("mailto:{$supportemail}", $supportemail) : false,
@@ -75,7 +81,7 @@ if ($form->is_cancelled()) {
 }
 
 echo $OUTPUT->header();
-
+echo $OUTPUT->render_from_template('user/form', $obj);
 echo $output;
 
 echo $OUTPUT->footer();
