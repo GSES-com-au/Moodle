aa6be943670ec25819c3dc2bee06df5d44c9a9bc
Marco <marco.cigna@gses.com.au>
2024-01-10 03:44:51 +0000

    Changes to learnr renderer

--- theme/learnr/renderers.php
+++ theme/learnr/renderers.php
@@ -1,26 +1,38 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/error.log');
+// defined('MOODLE_INTERNAL') || die();
 
-/**
- * Parent theme: Bootstrapbase by Bas Brands
- * Built on: Essential by Julian Ridden
- *
- * @package   theme_lambda
- * @copyright 2022 redPIthemes
- *
- */
+// class theme_overridden_renderer_factory_custom extends theme_overridden_renderer_factory {
 
-// require_once('renderers/qtype_multianswer_subq_renderer_base.php');
+// public function get_renderer(moodle_page $page, $component, $subtype = null, $target = null) {
+//     error_log("renderers.php has run #1");
+
+//     $classnames = $this->standard_renderer_classnames($component, $subtype);
+    
+//     list($target, $suffix) = $this->get_target_suffix($target);
+    
+//     // Theme lib.php and renderers.php files are loaded automatically
+//     // when loading the theme configs.
+    
+//     // First try the renderers with correct suffix.
+//     foreach ($this->prefixes as $prefix) {
+//     foreach ($classnames as $classnamedetails) {
+//     if ($classnamedetails['validwithprefix']) {
+//     if ($classnamedetails['autoloaded']) {
+//     $newclassname = $prefix . $classnamedetails['classname'] . $suffix;
+//     } else {
+//     $newclassname = $prefix . '_' . $classnamedetails['classname'] . $suffix;
+//     } error_log(var_export(($newclassname),true));
+//     if (class_exists($newclassname)) {
+//     return new $newclassname($page, $target);
+//     }
+//     }
+//     }
+//     }
+// }
+// }
+
+// 
